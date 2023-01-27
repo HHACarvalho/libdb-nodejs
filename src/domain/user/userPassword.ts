b504f934d2809c7280beb916bd2d5ac17a289c1e
Hugo Carvalho <hhac@protonmail.com>
2023-01-27 20:11:02 +0000

    Created UserPassword value object

---
+++ src/domain/user/userPassword.ts
@@ -0,0 +1,23 @@
+import { Guard } from '../../core/infrastructure/Guard';
+import { Result } from '../../core/infrastructure/Result';
+import { ValueObject } from '../../core/domain/ValueObject';
+
+interface IUserPassword {
+	value: string;
+}
+
+export class UserPassword extends ValueObject<IUserPassword> {
+	get value(): string {
+		return this.props.value;
+	}
+
+	public static create(password: string): Result<UserPassword> {
+		const guardResult = Guard.againstNullOrUndefined(password, 'password');
+
+		if (!guardResult.succeeded) {
+			return Result.fail<UserPassword>(guardResult.message);
+		}
+
+		return Result.ok<UserPassword>(new UserPassword({ value: password }));
+	}
+}
