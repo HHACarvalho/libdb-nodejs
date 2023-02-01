1360661d519f345e0e3e7bfd607eb4c791560ff5
Hugo Carvalho <hhac@protonmail.com>
2023-02-01 19:26:52 +0000

    Added a few more Guard methods

--- src/core/infrastructure/Guard.ts
+++ src/core/infrastructure/Guard.ts
@@ -11,8 +11,8 @@ export interface IGuardArgument {
 export type GuardArgumentCollection = IGuardArgument[];
 
 export class Guard {
-	public static againstNullOrUndefined(argument: any, argumentName: string): IGuardResult {
-		if (argument === null || argument === undefined) {
+	public static againstNullOrUndefined(value: any, argumentName: string): IGuardResult {
+		if (value === null || value === undefined) {
 			return { succeeded: false, message: `Guard: ${argumentName} is null or undefined` };
 		}
 
@@ -26,5 +26,42 @@ export class Guard {
 		}
 
 		return { succeeded: true };
+	}
+
+	public static againstInvalidLength(value: string, length: number, argumentName: string): IGuardResult {
+		if (value.length < length) {
+			return { succeeded: false, message: `Guard: ${argumentName} contains an invalid length` };
+		}
+
+		return { succeeded: true };
+	}
+
+	public static againstInvalidFormat(value: string, regex: RegExp, argumentName: string): IGuardResult {
+		if (!regex.test(value)) {
+			return { succeeded: false, message: `Guard: ${argumentName} contains an invalid format` };
+		}
+
+		return { succeeded: true };
+	}
+
+	public static againstNegativeNumber(argument: number, argumentName: string): IGuardResult {
+		if (argument < 0) {
+			return { succeeded: false, message: `Guard: ${argumentName} contains a negative value` };
+		}
+
+		return { succeeded: true };
+	}
+
+	public static isOneOf(value: any, validValues: any[], argumentName: string): IGuardResult {
+		for (let validValue of validValues) {
+			if (value === validValue) {
+				return { succeeded: true };
+			}
+		}
+
+		return {
+			succeeded: false,
+			message: `Guard: ${argumentName} isn't one of ${JSON.stringify(validValues)}. Got "${value}"`,
+		};
 	}
 }
