6d5efc33df8d1d7bc1262705399df903913fe4f2
Hugo Carvalho <hhac@protonmail.com>
2024-11-29 14:19:10 +0000

    Adds path and query parameters and request body validation

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -4,6 +4,21 @@ import IRoleService from '../services/IServices/IRoleService';
 
 import { Request, Response, Router } from 'express';
 import { inject, injectable } from 'inversify';
+import { z } from 'zod';
+
+const roleCreateBody = z.object({
+	name: z.string().min(2).max(32),
+	permissions: z.object({
+		manageMovies: z.boolean(),
+		manageRoles: z.boolean(),
+		manageUsers: z.boolean()
+	})
+});
+
+// const roleCreateBody = z.object({
+// 	name: z.string().min(2).max(32),
+// 	permissions: z.array(z.string())
+// });
 
 @injectable()
 export default class RoleController extends CoreController {
@@ -17,18 +32,19 @@ export default class RoleController extends CoreController {
 	public registerRoutes(): Router {
 		const router = Router();
 
-		router.post('', this.createRole.bind(this));
-		router.get('/all', this.findAllRoles.bind(this));
+		router.post('/', this.createRole.bind(this));
+		router.get('/', this.findAllRoles.bind(this));
 		router.get('/search', this.findRoles.bind(this));
-		router.get('/', this.findOneRole.bind(this));
-		router.put('', this.updateRole.bind(this));
-		router.delete('', this.deleteRole.bind(this));
+		router.get('/:roleName', this.findOneRole.bind(this));
+		router.put('/:roleName', this.updateRole.bind(this));
+		router.delete('/:roleName', this.deleteRole.bind(this));
 
 		return router;
 	}
 
 	private async createRole(req: Request, res: Response): Promise<void> {
-		await this.handleServiceCall(() => this.roleService.createRole(req.body), res);
+		const data = roleCreateBody.parse(req.body);
+		await this.handleServiceCall(() => this.roleService.createRole(data), res);
 	}
 
 	private async findAllRoles(req: Request, res: Response): Promise<void> {
@@ -36,25 +52,23 @@ export default class RoleController extends CoreController {
 	}
 
 	private async findRoles(req: Request, res: Response): Promise<void> {
-		await this.handleServiceCall(() => this.roleService.findRoles('Example'), res);
+		const roleName = (req.query.roleName as string) || '';
+		await this.handleServiceCall(() => this.roleService.findRoles(roleName), res);
 	}
 
 	private async findOneRole(req: Request, res: Response): Promise<void> {
-		await this.handleServiceCall(() => this.roleService.findOneRole('ExampleRole'), res);
+		const { roleName } = req.params;
+		await this.handleServiceCall(() => this.roleService.findOneRole(roleName), res);
 	}
 
 	private async updateRole(req: Request, res: Response): Promise<void> {
-		await this.handleServiceCall(() => this.roleService.updateRole('ExampleRole', req.body), res);
+		const { roleName } = req.params;
+		const data = roleCreateBody.parse(req.body);
+		await this.handleServiceCall(() => this.roleService.updateRole(roleName, data), res);
 	}
 
 	private async deleteRole(req: Request, res: Response): Promise<void> {
-		await this.handleServiceCall(() => this.roleService.deleteRole('ExampleRole'), res);
+		const { roleName } = req.params;
+		await this.handleServiceCall(() => this.roleService.deleteRole(roleName), res);
 	}
 }
-
-//import { z } from 'zod';
-
-// const roleCreateBody = z.object({
-// 	name: z.string().min(2).max(32),
-// 	permissions: z.array(z.string())
-// });
