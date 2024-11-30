6a191c4e628f4eb70aa852eb26df8a16cac2eb14
Hugo Carvalho <hhac@protonmail.com>
2024-11-30 14:07:26 +0000

    Deletes all the dependency loaders and adds them directly on the app's entrypoint

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,37 @@
 import 'reflect-metadata';
 
 import config from '../config';
-import Logger from './core/loaders/loggerLoader';
+import Container from './core/dependencies';
+import RoleController from './controllers/roleController';
+import UserController from './controllers/userController';
+import Logger from './core/logger';
 
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
+import { connect } from 'mongoose';
 
 async function startServer() {
+	// Connect to the MongoDB database
+	await connect(config.databaseURL);
+
 	const app = express();
 
-	await require('./core/loaders').default(app);
+	// Status endpoint
+	app.get('/status', (req, res) => res.sendStatus(200));
+
+	// Middlewares
+	app.use(cors());
+	app.use(cookieParser());
+	app.use(bodyParser.json());
+
+	// Load routes
+	app.use('/role', Container.resolve(RoleController).registerRoutes());
+	app.use('/user', Container.resolve(UserController).registerRoutes());
+
+	// Unknown endpoint
+	app.use((req, res) => res.sendStatus(404));
 
 	app
 		.listen(config.apiPort, () => {
--- src/core/loaders/mongooseLoader.ts
+++
@@ -1,8 +0,0 @@
-import config from '../../../config';
-
-import { connect, set } from 'mongoose';
-
-export default async () => {
-	set('strictQuery', false);
-	await connect(config.databaseURL);
-};
