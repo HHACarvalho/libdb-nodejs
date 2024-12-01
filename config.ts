76adcd64cf4eaf6ee51e04609c006db310e973ed
Hugo Carvalho <hhac@protonmail.com>
2024-12-01 00:15:29 +0000

    Adds dependency types to config.ts

--- config.ts
+++ config.ts
@@ -2,6 +2,15 @@ import { config } from 'dotenv';
 
 config();
 
+export const TYPES = {
+	IRoleSchema: Symbol.for('IRoleSchema'),
+	IRoleRepo: Symbol.for('IRoleRepo'),
+	IRoleService: Symbol.for('IRoleService'),
+	IUserSchema: Symbol.for('IUserSchema'),
+	IUserRepo: Symbol.for('IUserRepo'),
+	IUserService: Symbol.for('IUserService')
+};
+
 export default {
 	// Port used by the API
 	apiPort: 3000,
@@ -9,38 +18,18 @@ export default {
 	// MongoDB's connection string
 	databaseURL:
 		process.env.ENVIRONMENT === 'PRODUCTION'
-			? process.env.DB_CONNECTION_PRODUCTION
-			: process.env.DB_CONNECTION_DEVELOPMENT,
+			? process.env.DB_CONNECTION_PRODUCTION || ''
+			: process.env.DB_CONNECTION_DEVELOPMENT || '',
 
 	// JSON Web Token's expiration time in seconds
 	jwtDuration: 30,
 
 	// JSON Web Token's access secret
-	jwtAccessSecret: process.env.JWT_SECRET_ACCESS,
+	jwtAccessSecret: process.env.JWT_SECRET_ACCESS || '',
 
 	// JSON Web Token's refresh secret
-	jwtRefreshSecret: process.env.JWT_SECRET_REFRESH,
+	jwtRefreshSecret: process.env.JWT_SECRET_REFRESH || '',
 
 	// Default user role for new users
-	defaultRole: 'User',
-
-	controllers: {
-		role: 'roleController',
-		user: 'userController'
-	},
-
-	services: {
-		role: 'roleService',
-		user: 'userService'
-	},
-
-	repos: {
-		role: 'roleRepo',
-		user: 'userRepo'
-	},
-
-	schemas: {
-		role: 'roleSchema',
-		user: 'userSchema'
-	}
+	defaultRole: 'User'
 };
