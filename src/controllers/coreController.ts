8e90c7cb50f3a8dc9426dea0fc6f04e484db58fb
Hugo Carvalho <hhac@protonmail.com>
2024-12-04 19:43:49 +0000

    Allows coreController to return cookies

--- src/controllers/coreController.ts
+++ src/controllers/coreController.ts
@@ -1,3 +1,4 @@
+import config from '../../config';
 import Result from '../core/result';
 
 import { Response } from 'express';
@@ -6,15 +7,19 @@ export default abstract class CoreController {
 	protected async handleServiceCall(serviceMethod: () => Promise<Result>, res: Response) {
 		try {
 			const result = await serviceMethod();
+			res.status(result.statusCode);
+
 			if (!result.isSuccess) {
-				res.status(result.statusCode);
 				res.json({ error: result.error });
-			} else if (result.value === null) {
-				res.status(result.statusCode);
-				res.send();
 			} else {
-				res.status(result.statusCode);
-				res.json(result.value);
+				if (result.value == null) {
+					res.send();
+				} else if (typeof result.value === 'string') {
+					res.cookie('token', result.value, { httpOnly: true, maxAge: config.JWT_DURATION * 1000 });
+					res.send();
+				} else {
+					res.json(result.value);
+				}
 			}
 		} catch (error) {
 			console.error(error);
